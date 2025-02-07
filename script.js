27b501de7364a0ae430f292fb0b3db271ca9a082
HemanthBangera <hemanthbangera843@gmail.com>
2025-02-07 18:08:08 +0530

    Completed task 3

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-
+async function fetchUserData(){
+    try{
+        const data = await fetch("https://randomuser.me/api/")
+        const jsonify = await data.json();
+        console.log(jsonify)
+        console.log(jsonify.results[0].login.username)
+        console.log(jsonify.results[0].email)
+        document.getElementById("username").innerHTML = jsonify.results[0].login.username;
+        document.getElementById("email").innerHTML = jsonify.results[0].email;
+    }
+    catch(err){
+        console.log("Error fetching the data:",err);
+        document.getElementById("username").innerHTML = "Failed to load the username";
+        document.getElementById("email").innerHTML = "Failed to load the email";
+    }
+    
+}
